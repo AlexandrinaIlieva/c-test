99fce18cd8956a7c013059eb23155865f60a72c6
23-24 <23-24@PC08>
2023-11-23 11:17:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Diskriminanta
+
+            int a;
+            int b;
+            int c;
+            int D;
+
+            Console.WriteLine("Моля, въведете стойност на a");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Моля, въведете стойност на b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Моля, въведете стойност на c");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            D = b * b - 4 * a * c;
+
+            Console.WriteLine(D);
+
+            if (D > 0)
+            {
+                Console.WriteLine(D);
+            }
+            else if (D < 0)
+                    {
+                Console.WriteLine("Няма реални корени");
+            }
+            
+        }
+    }
+}
